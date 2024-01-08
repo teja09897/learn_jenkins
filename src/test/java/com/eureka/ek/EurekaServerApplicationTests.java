1c8c61f2d204da6c949d7ba48a27eb69668f84d2
RaviTeja <tejavasa0987@gmail.com>
2024-01-08 21:38:28 +0530

    eureka server configured

---
+++ src/test/java/com/eureka/ek/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.ek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
